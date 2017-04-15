bbeeec795d29d5072f900c511c1aab25a89d56e5
tolotrasam <tolotrasam@gmail.com>
2017-04-16 01:09:45 +0400

    token

--- index.js
+++ index.js
@@ -1,30 +1,25 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const request = require('request');
+var express = require("express");
+var request = require("request");
+var bodyParser = require("body-parser");
 
-//port initialisation
-app.set('port', (process.env.PORT || 5000))
+var app = express();
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+app.listen((process.env.PORT || 5000));
 
-// Process application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({extended: false}))
+// Server index page
+app.get("/", function (req, res) {
+    res.send("Deployed!");
+});
 
-// Process application/json
-app.use(bodyParser.json())
-
-// for Facebook verification
-app.get('/webhook/', function (req, res) {
-	if (req.query['hub.verify_token'] === process.env.VERIFICATION_TOKEN) {
-		res.send(req.query['hub.challenge'])
-	}
-	res.send('Error, wrong token')
-})
-
-//heroku homepage
-app.get('/', function(req, res){
-  res.send("Hi!");
-})
-
-// Spin up the server
-app.listen(app.get('port'), function() {
-	console.log('running on port', app.get('port'))
-})
+// Facebook Webhook
+// Used for verification
+app.get("/webhook", function (req, res) {
+    if (req.query["hub.verify_token"] === "this_is_my_token") {
+        console.log("Verified webhook");
+        res.status(200).send(req.query["hub.challenge"]);
+    } else {
+        console.error("Verification failed. The tokens do not match.");
+        res.sendStatus(403);
+    }
+});
