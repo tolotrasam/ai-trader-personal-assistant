15dbbce727187e1aa24983235bf53714552fd56c
tolotrasam <tolotrasam@gmail.com>
2017-04-20 00:37:20 +0400

    button

--- index.js
+++ index.js
@@ -51,9 +51,9 @@ function decideMessage(sender, text) {
     if(text ==='pregnant'){
         sendTextMessage(sender, "Sexual rapport")
     }else {
+        sendButtonMessage(sender,text)
     }
 
-    sendButtonMessage(sender,text)
 }
 app.post('/webhook/', function (req, res) {
     let messaging_events = req.body.entry[0].messaging
