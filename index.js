0a99eaac58f41a083de0afe51b6db4998914e9db
madagasikara <rtojosoa15@alustudent.com>
2017-04-24 22:19:59 +0400

    +3

--- index.js
+++ index.js
@@ -56,6 +56,7 @@ app.post('/webhook/', function (req, res) {
             if (event.postback) {
                 let text = JSON.stringify(event.postback)
                 console.log("THE POSTBACK TEXT IS: "+ text)
+                console.log("And the payload" + event.postback.payload)
                 decideMessage(sender, text)
                 continue
             }
