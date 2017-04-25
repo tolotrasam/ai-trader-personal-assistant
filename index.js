7ac64f959f8c901c20176118864342637ea546a7
madagasikara <rtojosoa15@alustudent.com>
2017-04-25 12:06:18 +0400

    +4

--- index.js
+++ index.js
@@ -266,7 +266,7 @@ function sendGetStarted() {
   }
   callGreetingAPI(greeting)
 }
-/*
+
 function sendGreeting() {
   var greeting = {
     setting_type: "greeting",
@@ -275,7 +275,7 @@ function sendGreeting() {
     }
   };
   callGreetingAPI(greeting)
-}*/
+}
 
 // Calls the Facebook graph api to change various bot settings
 function facebookThreadAPI(jsonFile, cmd){
