50c7566edd2dc342f1e89e6593faba097b6f012b
tolotrasam <tolotrasam@gmail.com>
2017-04-26 01:50:53 +0400

    database fetching test

--- index.js
+++ index.js
@@ -129,9 +129,9 @@ function decideMessage(sender, text) {
 function getMovieDetail(userId, field) {
     Movie.findOne({user_id: userId}, function(err, movie) {
         if(err) {
-            sendMessage(userId, {text: "Something went wrong. Try again"});
+            sendTextMessage(userId, {text: "Something went wrong. Try again"});
         } else {
-            sendMessage(userId, {text: movie[field]});
+            sendTextMessage(userId, {text: movie[field]+' sent from mongo DB'});
         }
     });
 }
