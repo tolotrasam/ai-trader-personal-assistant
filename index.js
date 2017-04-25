8cd25af5f36e6e8595b644725e4fd9f0c6b7959b
madagasikara <rtojosoa15@alustudent.com>
2017-04-25 12:02:05 +0400

    +4

--- index.js
+++ index.js
@@ -281,8 +281,8 @@ function sendGreeting() {
 function facebookThreadAPI(jsonFile, cmd){
     // Start the request
     request({
-        url: 'https://graph.facebook.com/v2.6/me/thread_settings',
-        qs: {access_token: token},
+        uri: 'https://graph.facebook.com/v2.6/me/thread_settings',
+        qs: { access_token: token},
         method: 'POST',
         headers: {'Content-Type': 'application/json'},
         form: require(jsonFile)
