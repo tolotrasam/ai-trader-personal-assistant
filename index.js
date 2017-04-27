40d50db8d6aa3266ccff57552cab7b8f43b8fec2
madagasikara <rtojosoa15@alustudent.com>
2017-04-28 00:31:58 +0400

    quick replies OPERATIONAL

--- index.js
+++ index.js
@@ -197,7 +197,7 @@ function decideMessagePostBack(sender, raw_postback) {
     if(raw_postback == 'get_started') {    
         //sendTextMessage(sender, "Hello there!")
         //before proceeding, check if user in database:
-        sendQuickReply(sender, "Select your age range: ", "text", "less than 18", "minor," "text", "more than 18", "major");
+        sendQuickReply(sender, "Select your age range: ", "text", "less than 18", "minor", "text", "more than 18", "major");
         insertToSession(sender) // insert to session if not yet in there
         if (userData.sender.isAnswering) {
             if (userData.sender.payload === 'age') {
