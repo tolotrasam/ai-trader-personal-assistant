9e5c72c696feab4b68c326d03595328a2b79e6f2
tolotrasam <tolotrasam@gmail.com>
2017-04-27 14:34:56 +0400

    new file

--- index.js
+++ index.js
@@ -212,7 +212,7 @@ function decideMessage(sender, text) {
         poster_url:'nice url .com'
     };
     var options = {upsert: true};
-    Movie.findOneAndUpdate(query, update, options, function(err, mov) {
+    Users.findOneAndUpdate(query, update, options, function(err, mov) {
         if (err) {
             console.log("Database error: " + err);
         } else {
