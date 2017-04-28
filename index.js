6f4b1eeaed751dd545d4e14cf9339b3d203a37b4
tolotrasam <tolotrasam@gmail.com>
2017-04-29 02:09:53 +0400

    nene evve

--- index.js
+++ index.js
@@ -187,7 +187,8 @@ function get_more_content(sender, content_target_id, id_in_group) {
     console.log('getting to sender: '+sender+ ' id: '+content_target_id+ ' id in group '+ id_in_group  );
     if (!(typeof (id_in_group) === 'undefined')) {
         console.log(id_in_group, 'id in group');
-        Content.findOne({content_id: content_target_id, id_in_group: id_in_group}, function (err, chat_content) {
+        // Content.findOne({content_id: content_target_id, id_in_group: id_in_group}, function (err, chat_content) {
+        Content.findOne({id_in_group: id_in_group}, function (err, chat_content) {
             if (err) {
                 sendTextMessage(sender, "Sorry, I couldn't get what you asked for the moment. Try out later");
             } else {
